ccecde97ed36d1e0adbd8c4e748a27febd0ec450
teacher Li <1227380089@qq.com>
2019-12-12 10:28:24 +0800

    Create company.py

---
+++ company.py
@@ -0,0 +1,1 @@
+[球面三角形距离公式推导](https://dothinking.github.io/blog/2017/03/08/%E7%90%83%E9%9D%A2%E8%B7%9D%E7%A6%BB%E4%B8%8E%E6%96%B9%E4%BD%8D%E8%A7%92%E5%85%AC%E5%BC%8F%E7%9A%84%E6%8E%A8%E5%AF%BC-%E8%A7%A3%E4%B8%89%E8%A7%92%E5%BD%A2%E6%B3%95.html)
